823d6e0e7e68f58a73b96f6e0532e9443209bf57
ggvhcs <ggvhcs@gmail.com>
2025-02-23 17:20:32 -0500

    february 23, 2025.

--- docker/testdbdump.sql
+++
@@ -1,6 +0,0 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE table_test(timestamp TEXT, description TEXT);
-INSERT INTO table_test VALUES('2025-02-18 17:58:33','First test data. Foo');
-INSERT INTO table_test VALUES('2025-02-18 17:58:39','Second test data. Bar');
-COMMIT;
